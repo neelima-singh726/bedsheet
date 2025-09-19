c703a009f773e82f29e9922f756be790135a3232
neelima-singh726 <59680731+neelima-singh726@users.noreply.github.com>
2025-09-19 12:53:05 +0530

    Add files via upload
    .

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: "/Users/neelimasingh/Downloads/BedsheetWeb/beds-web",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
