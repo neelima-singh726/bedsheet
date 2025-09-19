93f2377ffd48e742e389f14b172b4c491ac133a6
neelima-singh726 <59680731+neelima-singh726@users.noreply.github.com>
2025-09-19 13:54:41 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: "/Users/neelimasingh/Downloads/BedsheetWeb/beds-web",
   eslint: {
     ignoreDuringBuilds: true,
   },
